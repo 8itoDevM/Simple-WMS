225a0ea73d9a4fd4bb09594615004ae1d5a0b335
Gustavo Marcolino <gustavomarcolino862@gmail.com>
2024-09-29 17:57:07 -0300

    Updated .gitignore and added project files

---
+++ SimpleWMS/DatabaseManager.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SimpleWMS
+{
+    public class DatabaseManager
+    {
+        private string connectionString;
+
+        public DatabaseManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        public int CountItems()
+        {
+            int totalItems = 0;
+            string query = "SELECT itemQty FROM Items";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            totalItems += reader.GetInt32(0);
+                        }
+                    }
+                }
+            }
+            return totalItems;
+        }
+
+        public void ShowUsers()
+        {
+            string query = "SELECT Name FROM Users";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("Showing all users");
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+        }
+
+        public void RegisterItems(string itemName, int itemQty)
+        {
+            string query = "INSERT INTO Items (itemName, itemQty) VALUES (@itemName, @itemQty)";
+            string verifySelect = "SELECT itemName from Items where itemName = @itemName";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new MySqlCommand(verifySelect, connection))
+                {
+                    command.Parameters.AddWithValue("@itemName", itemName);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            Console.WriteLine("Err: Item already exists");
+                        } else
+                        {
+                            reader.DisposeAsync();
+                            using (MySqlCommand commandQuery = new MySqlCommand(query, connection))
+                            {
+                                commandQuery.Parameters.AddWithValue("@itemName", itemName);
+                                commandQuery.Parameters.AddWithValue("@itemQty", itemQty);
+
+                                commandQuery.ExecuteNonQuery();
+                                Console.WriteLine("Item inserted successfully.");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void AddItem(string itemName, int itemQty)
+        {
+            string query = "UPDATE Items SET itemQty = itemQty + @itemQty WHERE itemName = @itemName";
+            string verifySelect = "SELECT itemName FROM Items WHERE itemName = @itemName";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(verifySelect, connection))
+                {
+                    command.Parameters.AddWithValue("@itemName", itemName);
+                    command.Parameters.AddWithValue("@itemQty", itemQty);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("Err: Item doesn't exists");
+                        }
+                        else if (reader.HasRows)
+                        {
+                            reader.DisposeAsync();
+                            using (MySqlCommand commandUpdate = new MySqlCommand(query, connection))
+                            {
+                                commandUpdate.Parameters.AddWithValue("@itemName", itemName);
+                                commandUpdate.Parameters.AddWithValue("@itemQty", itemQty);
+
+                                commandUpdate.ExecuteNonQuery();
+                                Console.WriteLine("Item inserted successfully!");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ShipItem(string itemName, int itemQty)
+        {
+            string query = "UPDATE Items SET itemQty = itemQty - @itemQty WHERE itemName = @itemName";
+            string verifySelect = "SELECT itemName, itemQty from Items where itemName = @itemName";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                using (MySqlCommand command = new MySqlCommand(verifySelect, connection))
+                {
+                    command.Parameters.AddWithValue("@itemName", itemName);
+                    command.Parameters.AddWithValue("@itemQty", itemQty);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("Err: Item doesn't exists");
+                        } else
+                        {
+                            reader.Read();
+
+                            if (reader.GetInt32(1) >= itemQty)
+                            {
+                                reader.DisposeAsync();
+                                using (MySqlCommand commandUpdate = new MySqlCommand(query, connection))
+                                {
+                                    commandUpdate.Parameters.AddWithValue("@itemName", itemName);
+                                    commandUpdate.Parameters.AddWithValue("@itemQty", itemQty);
+
+                                    commandUpdate.ExecuteNonQuery();
+                                    Console.WriteLine("Item shipped successfully!");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Err: Not enough of {reader.GetString(0)} in stock - {reader.GetInt32(1)} left");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SimpleWMS/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace SimpleWMS
+{
+    internal class Program
+    {
+        static int Menu()
+        {
+            int i;
+            Console.WriteLine("Count items - 1");
+            Console.WriteLine("Show users - 2");
+            Console.WriteLine("Register item - 3");
+            Console.WriteLine("Add items - 4");
+            Console.WriteLine("Ship Item - 5");
+            Console.WriteLine("Exit - 0");
+
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            return i;
+        }
+
+        static void CountItems(DatabaseManager dbManager)
+        {
+            int qty = dbManager.CountItems();
+            Console.WriteLine($"There are {qty} items in stock");
+        }
+
+        static void ShowUsers(DatabaseManager dbManager)
+        {
+            dbManager.ShowUsers();
+        }
+
+        static void RegisterItem(DatabaseManager dbManager)
+        {
+            string itmName = "default";
+            int itmInit = 0;
+
+            Console.WriteLine("Registering an item");
+            Console.WriteLine(" ");
+
+            Console.Write("Item name: ");
+            itmName = Console.ReadLine();
+
+            Console.Write("Item initial quantity: ");
+            itmInit = Convert.ToInt32(Console.ReadLine());
+
+            dbManager.RegisterItems(itmName, itmInit);
+        }
+
+        static void AddItem(DatabaseManager dbManager)
+        {
+            string itmName = "default";
+            int itmQty = 0;
+
+            Console.WriteLine("Adding an Item");
+            Console.WriteLine(" ");
+
+            Console.Write("Item name: ");
+            itmName = Console.ReadLine();
+
+            Console.Write("Quantity to be added: ");
+            itmQty = Convert.ToInt32(Console.ReadLine());
+
+            dbManager.AddItem(itmName, itmQty);
+        }
+
+        static void ShipItems(DatabaseManager dbManager)
+        {
+            string itmName = "default";
+            int itmQty = 0;
+
+            Console.WriteLine("Shipping an Item");
+            Console.WriteLine(" ");
+
+            Console.Write("Item name: ");
+            itmName = Console.ReadLine();
+
+            Console.Write("Quantity to be shipped: ");
+            itmQty = Convert.ToInt32(Console.ReadLine());
+
+            dbManager.ShipItem(itmName, itmQty);
+        }
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("MySqlDatabase");
+
+            DatabaseManager dbManager = new DatabaseManager(connectionString);
+
+            bool running = true;
+
+            while (running)
+            {
+                switch (Menu())
+                {
+                    case 0:
+                        Console.Clear();
+                        running = false;
+
+                        break;
+                    case 1:
+                        Console.Clear();
+
+                        CountItems(dbManager);
+
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Clear();
+
+                        ShowUsers(dbManager);
+
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Clear();
+
+                        RegisterItem(dbManager);
+
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Clear();
+
+                        AddItem(dbManager);
+
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        Console.Clear();
+
+                        ShipItems(dbManager);
+
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.Clear();
+
+                        Console.WriteLine("Invalid choice. Please try again.");
+
+                        Console.ReadLine();
+                        break;
+                }
+
+                Console.Clear();
+            }
+
+            Console.Write("Press any Key to exit ");
+            Console.ReadLine();
+        }
+    }
+}
